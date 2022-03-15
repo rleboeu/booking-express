49fe107c04f0937ec3dc89fd32d3490d1740e35e
rleboeu <leboeufrm@gmail.com>
2022-03-15 12:10:37 -0400

    Created BookingDriver.java stub

---
+++ express/BookingDriver.java
@@ -0,0 +1,18 @@
+package express;
+
+/**
+ * @author Miller Banford, Andrew Garcia, Thomas Ferguson, Ryan LeBoeuf
+ * @version 1.0.0
+ */
+public class BookingDriver {
+    
+    /**
+     * Constructor
+     */
+    public BookingDriver() {}
+
+    public static void main(String[] args) {
+        // TO-DO your mom
+    }
+
+}
