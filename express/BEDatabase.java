50ea579cb621fe159f58fa0ab71b42455e14fc3e
rleboeu <leboeufrm@gmail.com>
2022-03-15 12:28:10 -0400

    Create BEDatabase.java

---
+++ express/BEDatabase.java
@@ -0,0 +1,17 @@
+package express;
+
+/**
+ * Database class
+ * @author rleboeu
+ * @version 1.0.0
+ */
+public class BEDatabase {
+    
+    /**
+     * Constructor
+     */
+    public BEDatabase() {
+
+    }
+
+}
