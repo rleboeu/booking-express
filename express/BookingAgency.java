3876a60e24a2dfde99ac812b04234d912ae11857
andyg817 <andrewag02@outlook.com>
2022-03-17 12:00:39 -0400

    andy- stuff

--- express/BookingAgency.java
+++ express/BookingAgency.java
@@ -10,17 +10,35 @@ public class BookingAgency {
     String AirportCode;
     ArrayList<BookableEntity> bookings = new ArrayList<BookableEntity>();
     
+/**
+ * Contructor for booking Agency
+ * @param name name of booking agency
+ * @param AirportCode airport code 
+ * @param bookings bookings in agency
+ */
     public BookingAgency(String name, String AirportCode, ArrayList<BookableEntity> bookings) {
         this.name = name;
         this.AirportCode = AirportCode;
         this.bookings = bookings;
     }
+/**
+ * Contructor for booking agency
+ * @param name name of agency 
+ */
     public BookingAgency(String name) {
         this.name = name;
     }
+/**
+ * Adds booking to booking agency
+ * @param booking bookings in list
+ */
     public void addBooking(BookableEntity booking) {
         bookings.add(booking);
     }
+/**
+ * Removes booking from agency
+ * @param booking bookings in list
+ */
     public void removeBooking(BookableEntity booking) {
         bookings.remove(booking);
     }
