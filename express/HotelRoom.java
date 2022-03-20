9f08cfb37d1b29bc0ec308fb94c4f4dc3f5cd7e7
tfergu72 <70164402+tfergu72@users.noreply.github.com>
2022-03-20 15:47:23 -0400

    HotelRoom

--- express/HotelRoom.java
+++ express/HotelRoom.java
@@ -8,10 +8,17 @@ public class HotelRoom extends BookableEntity {
     private LocalDate availabilityStart;
     private LocalDate availabilityEnd;
     private int numBeds;
-
+    private ArrayList<Review> reviews;
+/**
+ * Constructor for HotelRoom
+ * @param availabilityStart Initializes the date of the hotel availability
+ * @param availabilityEnd Initializes the date of the hotel availabilities end
+ * @param reviews Initializes arraylist of reviews
+ */
     public HotelRoom(LocalDate availabilityStart, LocalDate availabilityEnd, ArrayList<Review> reviews){
         this.availabilityStart = availabilityStart;
         this.availabilityEnd = availabilityEnd;
-        this.numBeds = numBeds;
+        numBeds = 0;
+        this.reviews = reviews;
     }
 }
