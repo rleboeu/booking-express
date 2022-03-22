f8d156a2224bb7d63971406ace15360d9599eae4
rleboeu <leboeufrm@gmail.com>
2022-03-22 14:29:02 -0400

    Added second constructor for Passport to be used if a UUID has already been generated

--- express/Passport.java
+++ express/Passport.java
@@ -19,8 +19,27 @@ public class Passport {
     private String dateExpiration;
     public static DateTimeFormatter dateFormatter;
     private ArrayList<String> destinationHistory = new ArrayList<String>();
-/**
+
+    /**
+     * Constructor for Passport if no UUID is specified
+     * @param firstName of the person
+     * @param lastName of the person
+     * @param DOB of the person
+     * @param nationality of the person
+     * @param POB of the person
+     * @param sex of the person
+     * @param dateIssued of the passport
+     * @param dateExpiration of the passport
+     * @param destinationHistory of the passport/person
+     */
+    public Passport(String firstName, String lastName, String DOB, String nationality, String POB, 
+    Sex sex, String dateIssued, String dateExpiration, ArrayList<String> destinationHistory) {
+        this(UUID.randomUUID(), firstName, lastName, DOB, nationality, POB, sex, dateIssued, dateExpiration, destinationHistory);
+    }
+
+    /**
  * Contructor for Passport
+ * @param id UUID of the passport
  * @param firstName first name of person passport
  * @param lastName last name of person passport
  * @param DOB date of birth of person
@@ -31,9 +50,9 @@ public class Passport {
  * @param dateExpiration date passport expired
  * @param destinationHistory destination history on passport
  */
-    public Passport(String firstName, String lastName, String DOB, String nationality, String POB, 
-    Sex sex, String dateIssued, String dateExpiration, String passportNum, ArrayList<String> destinationHistory) {
-        this.id = UUID.randomUUID();
+    public Passport(UUID id, String firstName, String lastName, String DOB, String nationality, String POB, 
+    Sex sex, String dateIssued, String dateExpiration, ArrayList<String> destinationHistory) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.DOB = DOB;
