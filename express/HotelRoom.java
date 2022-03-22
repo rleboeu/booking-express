5e0c3379452d1f3f0485ccd78d4bc3f09e0005e3
tfergu72 <70164402+tfergu72@users.noreply.github.com>
2022-03-22 12:41:24 -0400

    Added what I lost when had to reclone repo

--- express/HotelRoom.java
+++ express/HotelRoom.java
@@ -12,6 +12,7 @@ public class HotelRoom extends BookableEntity {
     private ArrayList<Review> reviews;
 /**
  * Constructor for HotelRoom
+ * @param nearAirportCode Initializes the nearby airport code
  * @param availabilityStart Initializes the date of the hotel availability
  * @param availabilityEnd Initializes the date of the hotel availabilities end
  * @param reviews Initializes arraylist of reviews
@@ -19,8 +20,38 @@ public class HotelRoom extends BookableEntity {
     public HotelRoom(int nearAirportCode, LocalDate availabilityStart, LocalDate availabilityEnd, ArrayList<Review> reviews){
         this.availabilityStart = availabilityStart;
         this.availabilityEnd = availabilityEnd;
-        numBeds = 0;
         this.reviews = reviews;
         this.nearAirportCode = nearAirportCode;
     }
+public LocalDate getAvailabilityStart() {
+    return availabilityStart;
+}
+public void setAvailabilityStart(LocalDate availabilityStart) {
+    this.availabilityStart = availabilityStart;
+}
+public LocalDate getAvailabilityEnd() {
+    return availabilityEnd;
+}
+public void setAvailabilityEnd(LocalDate availabilityEnd) {
+    this.availabilityEnd = availabilityEnd;
+}
+public int getNumBeds() {
+    return numBeds;
+}
+public void setNumBeds(int numBeds) {
+    this.numBeds = numBeds;
+}
+public int getNearAirportCode() {
+    return nearAirportCode;
+}
+public void setNearAirportCode(int nearAirportCode) {
+    this.nearAirportCode = nearAirportCode;
+}
+public ArrayList<Review> getReviews() {
+    return reviews;
+}
+public void setReviews(ArrayList<Review> reviews) {
+    this.reviews = reviews;
+}
+    
 }
