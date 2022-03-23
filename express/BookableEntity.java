7766c1380f8769636057198d75f9ec0d95395fcb
rleboeu <leboeufrm@gmail.com>
2022-03-23 11:13:35 -0500

    Implemented getUUID()

--- express/BookableEntity.java
+++ express/BookableEntity.java
@@ -10,7 +10,15 @@ public abstract class BookableEntity {
     protected UUID uuid = UUID.randomUUID();
     protected double price;
     protected boolean available;
-    protected ArrayList<Review> reviews = new ArrayList<Review>();
+    protected ArrayList<Review> reviews;
+
+    /**
+     * Accessor for UUID
+     * @return UUID
+     */
+    public UUID getUUID() {
+        return this.uuid;
+    }
 
     /**
      * Reuturns if available 
@@ -47,12 +55,12 @@ public abstract class BookableEntity {
         }
     }
 
-/**
- * Adds review to review ArrayList
- * @param firstName first name of review
- * @param rating rating of reivew
- * @param comments comment on reivew
- */
+    /**
+     * Adds review to review ArrayList
+     * @param firstName first name of review
+     * @param rating rating of reivew
+     * @param comments comment on reivew
+     */
     public void addReview(String firstName, double rating, String comments) {
         Review review = new Review(firstName, rating, comments);
         reviews.add(review);
