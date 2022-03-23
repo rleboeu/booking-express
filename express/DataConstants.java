bee2bc2c161d7cadbdb4c4821d92e40e3ebd8ab8
rleboeu <leboeufrm@gmail.com>
2022-03-23 11:10:26 -0500

    Added all constants

--- express/DataConstants.java
+++ express/DataConstants.java
@@ -29,8 +29,43 @@ public abstract class DataConstants {
     protected static final String USER_BOOKING_HISTORY = "bookingHistory";
     protected static final String USER_PASSPORTS = "passports";
 
+    // Constants for flights.json
+    protected static final String FLIGHT_DEPARTURE_TIME = "departureTime";
+    protected static final String FLIGHT_ARRIVAL_TIME = "arrivalTime";
+    protected static final String FLIGHT_DEPARTURE_CODE = "departureCode";
+    protected static final String FLIGHT_ARRIVAL_CODE = "arrivalCode";
+    protected static final String FLIGHT_PRICE = "price";
+    protected static final String FLIGHT_SEAT_MAP = "seatMap";
+    protected static final String FLIGHT_REVIEWS = "reviews";
+
+    // Constants for hotelrooms.json
+    protected static final String HOTEL_ROOM_NUMBER = "roomNumber";
+    protected static final String HOTEL_PRICE = "pricePerNight";
+    protected static final String HOTEL_AVAIL_START = "availabilityStart";
+    protected static final String HOTEL_AVAIL_END = "availabilityEnd";
+    protected static final String HOTEL_NUM_BEDS = "numBeds";
+    protected static final String HOTEL_NEAR_AIRPORT_CODE = "nearAirportCode";
+    protected static final String HOTEL_AVAILABLE = "available";
+    protected static final String HOTEL_REVIEWS = "reviews";
+
+    // Constants for cars.json
+    protected static final String CAR_NAME = "name";
+    protected static final String CAR_PRICE = "price";
+    protected static final String CAR_STYLE = "style";
+    protected static final String CAR_FEATURES = "features";
+    protected static final String CAR_START_DAY = "startDay";
+    protected static final String CAR_END_DAY = "endDay";
+    protected static final String CAR_NUM_SEATS = "numSeats";
+    protected static final String CAR_AVAILABLE = "available";
+    protected static final String CAR_REVIEWS = "reviews";
+
     // Constants for agencies.json
     protected static final String AGENCY_NAME = "name";
     protected static final String AGENCY_BOOKINGS = "bookings";
+
+    // Constants for reviews
+    protected static final String REVIEW_RATING = "rating";
+    protected static final String REVIEW_NAME = "firstName";
+    protected static final String REVIEW_COMMENTS = "comments";
 
 }
