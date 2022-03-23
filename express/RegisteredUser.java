60aed25cc24edfc3fc87621ef5909e24592fe9c2
rleboeu <leboeufrm@gmail.com>
2022-03-23 11:17:40 -0500

    Now stores bookings and Passports as UUIDs instead of holding a copy

--- express/RegisteredUser.java
+++ express/RegisteredUser.java
@@ -16,23 +16,23 @@ public class RegisteredUser extends User {
     private String lastName;
     private int age;
     private boolean allowedToBook;
-    private ArrayList<Passport> passports;
+    private ArrayList<String> passports;
     private int numFlightsCurrentYear;
-    private ArrayList<BookableEntity> bookingHistory;
+    private ArrayList<String> bookingHistory;
     
     /**
-     * Constructer
-     * @param firstName String
-     * @param lastName String
-     * @param age int
-     * @param allowedToBook boolean
-     * @param passports ArrayList<Passport>
-     * @param numFlightsCurrentYear int
-     * @param bookingHistory ArrayList<BookableEntity>
-     * @param location String
+     * 
+     * @param id
+     * @param firstName
+     * @param lastName
+     * @param age
+     * @param allowedToBook
+     * @param passports
+     * @param bookingHistory
+     * @param location
      */
     public RegisteredUser(UUID id, String firstName, String lastName, int age, boolean allowedToBook,
-            ArrayList<Passport> passports, ArrayList<BookableEntity> bookingHistory, String location) {
+            ArrayList<String> passports, ArrayList<String> bookingHistory, String location) {
         super(location);
         this.id = id;
         this.firstName = firstName;
@@ -60,9 +60,9 @@ public class RegisteredUser extends User {
 
     /**
      * adds a booking to the bookingHistory Array List
-     * @param booking BookableEntity to be added
+     * @param booking String uuid of booking
      */
-    public void addBooking(BookableEntity booking) {
+    public void addBooking(String booking) {
         if (allowedToBook) {
             this.bookingHistory.add(booking);
         }
@@ -70,25 +70,25 @@ public class RegisteredUser extends User {
 
     /**
      * removes a booking to the bookingHistory Array List
-     * @param booking BookableEntity to be removed
+     * @param booking String uuid of booking to be removed
      */
-    public void removeBooking(BookableEntity booking) {
+    public void removeBooking(String booking) {
         bookingHistory.remove(booking);
     }
 
     /**
      * adds a passport to the passports Array List
-     * @param passport Passport to be added
+     * @param passport String uuid of passport to be added
      */
-    public void addPassport(Passport passport) {
+    public void addPassport(String passport) {
         passports.add(passport);
     }
 
     /**
      * removes a passport to the passports Array List
-     * @param passport Passport to be removed
+     * @param passport String uuid of passport to be removed
      */
-    public void removePassport(Passport passport) {
+    public void removePassport(String passport) {
         passports.remove(passport);
     }
     
