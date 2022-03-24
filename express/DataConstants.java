7c8d586d57767c2523cdcd5dbfd5f334d300c9cd
rleboeu <leboeufrm@gmail.com>
2022-03-24 11:30:46 -0500

    Update DataConstants.java

--- express/DataConstants.java
+++ express/DataConstants.java
@@ -19,7 +19,9 @@ public abstract class DataConstants {
     protected static final String ID = "id";
     protected static final String FIRST_NAME = "firstName";
     protected static final String LAST_NAME = "lastName";
-    
+    protected static final String NAME = "name";
+    protected static final String AVAILABLE = "available";
+
     // Constants for passports.json
     protected static final String PASSPORT_DATE_OF_BIRTH = "dateOfBirth";
     protected static final String PASSPORT_NATIONALITY = "nationality";
@@ -53,7 +55,6 @@ public abstract class DataConstants {
     protected static final String HOTEL_AVAIL_END = "availabilityEnd";
     protected static final String HOTEL_NUM_BEDS = "numBeds";
     protected static final String HOTEL_NEAR_AIRPORT_CODE = "nearAirportCode";
-    protected static final String HOTEL_AVAILABLE = "available";
     protected static final String HOTEL_REVIEWS = "reviews";
 
     // Constants for cars.json
@@ -64,7 +65,6 @@ public abstract class DataConstants {
     protected static final String CAR_START_DAY = "startDay";
     protected static final String CAR_END_DAY = "endDay";
     protected static final String CAR_NUM_SEATS = "numSeats";
-    protected static final String CAR_AVAILABLE = "available";
     protected static final String CAR_REVIEWS = "reviews";
 
     // Constants for agencies.json
